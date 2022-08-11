60ac9618f1fbeedf9a3dd9bf5a50e1142fd78570
pitsy <rainer.pits@gmail.com>
2022-08-11 09:25:19 +0300

    Exercise 1 - some basic html and css

---
+++ exercise_1/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        <button className='button1'>VAJUTA!</button>
+        <p className='t1'>norwegian wood</p>
+        <img className='img1' src="https://www.sciencefriday.com/wp-content/uploads/2021/12/Forest-trees.jpg" alt="" />
+      </body>
+    </div>
+  );
+}
+
+export default App;
