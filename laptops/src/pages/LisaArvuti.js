8fbd4f376b166aa93a9b2248b17ddf85d2e65403
pitsy <rainer.pits@gmail.com>
2022-08-11 12:59:07 +0300

    Exercise 4 completed, useState and functions

--- laptops/src/pages/LisaArvuti.js
+++ laptops/src/pages/LisaArvuti.js
@@ -1,8 +1,24 @@
+import { useState } from "react";
 
 function LisaArvuti() {
+    const [message, setMessage] = useState("Lisa arvuti!");
+    const [n2itaNuppu, uuendaN2itaNuppu] = useState(true);
+    
+    function addProduct() {
+        setMessage("Arvuti lisatud!");
+        uuendaN2itaNuppu(false);
+    }
+    
     return ( 
         <div>
-            Lisa arvuti
+            <div>Sonum: {message}</div>
+            <label>Mark</label> <br />
+            <input type="text" /> <br />
+            <label>Mudel</label> <br />
+            <input type="text" /> <br />
+            <label>Maksumus</label> <br />
+            <input type="number" /> <br />
+            { n2itaNuppu === true && <button onClick={() => addProduct()}>Sisesta</button> }
         </div>
      );
 }
