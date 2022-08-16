d04ddd27de2119c9eb05a21288575dfd403df047
pitsy <rainer.pits@gmail.com>
2022-08-16 09:29:47 +0300

    useState harjutused uudiste lehel

--- uudised/src/pages/Kontakt.js
+++ uudised/src/pages/Kontakt.js
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 
 function Kontakt() {
+
+    const [n2itaKristiine, muudaKristiine] = useState(false);
+    const [n2itaUlemiste, muudaUlemiste] = useState(false);
+    const [n2itaLasnamae, muudaLasnamae] = useState(false);
+
     return ( 
         <div>
             <div>See on kontaktide leht</div>
             <div>Vota meiega uhendust:</div> <br />
-            <div>Lasnamae Centrum</div>
-            <div>+37259999999</div>
-            <div>Pae 999</div>
+            <div onClick={() => muudaLasnamae(!n2itaLasnamae)}>Lasnamae Centrum</div>
+            {n2itaLasnamae && <div>+37259999999</div>}
+            <div>Pae 999</div><br />
+            <div onClick={() => muudaKristiine(!n2itaKristiine)}>Kristiine keskus</div>
+            {n2itaKristiine && <div>+69577777777</div>}
+            <div>Endla 455</div><br />
+            <div onClick={() => muudaUlemiste(!n2itaUlemiste)}>Ulemiste keskus</div>
+            {n2itaUlemiste && <div>+37156439812</div>}
+            <div>Suuri-Soyamae 4</div>
         </div> 
     );
 }
--- uudised/src/pages/Meist.js
+++ uudised/src/pages/Meist.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 
 function Meist() {
+
+    const [kontakt, n2itaKontakt] = useState('');
+
     return ( 
         <div>
             <div>See on meist leht</div> <br />
             <div>Me oleme toredad vennad</div> <br />
             <div>Bill Gates</div>
-            <div>+987654321</div>
+            <button onClick={() => n2itaKontakt('+987654321')}>Vota uhendust</button>
+            <div>Niisama on siin</div> <br />
+            <div>Arvo Part</div>
+            <button onClick={() => n2itaKontakt('+313131313')}>Vota uhendust</button>
+            <div>Klippide taustamuusika</div><br />
+            { kontakt !== '' && <div>Tema kontakt: {kontakt}</div>}
         </div> 
     );
 }
